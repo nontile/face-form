0f5182722d9b3907e4128722cbbac1f2810fc940
JKM <kyoungmin@flk.kr>
2021-01-14 16:10:01 +0900

    !

--- public/js/fileupload.js
+++ public/js/fileupload.js
@@ -4,7 +4,7 @@ function readURL(input) {
         reader.onload = function (e) {
             $('.image-upload-wrap').hide();
             $('.file-upload-image').attr('src', e.target.result);
-            
+            $('.file-upload-content').show();
             // $('.image-title').html(input.files[0].name);
         };
         reader.readAsDataURL(input.files[0]);
@@ -12,7 +12,6 @@ function readURL(input) {
             then(() => {
                 console.log("init")
                 predict();
-                $('.file-upload-content').show();
                 loadingHide();
             })
             .catch(err => console.log(err));
